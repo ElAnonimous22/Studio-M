9ef07c212bd873ded52aefb7f8ad3e55ebcbadc9
MenteDisciplinada <tutivenmatheo9@gmail.com>
2026-08-15 13:24:16 +0200

    Crear app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+{
+  id: "tu-nuevo-producto",
+  titulo: "Nombre del producto",
+  precio: "00.00€",
+  imagenPrincipal: "ruta/a/tu/foto.jpg",
+  marca: "Marca",
+  estado: "Nuevo / Usado",
+  talla: "L",
+  peso: "200 g",
+  material: "Algodón",
+  entrega: "Envío o en mano",
+  descripcion: "Escribe la descripción aquí.",
+  linkWallapop: "tu_enlace_wallapop",
+  linkVinted: "tu_enlace_vinted"
+}
